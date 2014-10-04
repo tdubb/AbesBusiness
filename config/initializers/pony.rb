1f1f6403d16554580f265e4f8408303606abfe12
twasden <wasdenonrails@gmail.com>
2014-10-04 16:29:17 -0500

    get sendgrid username, password

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -4,13 +4,13 @@ Pony.mail(
   :to => 'twasden@gmail.com',
   :via => :smtp,
   :via_options => {
-    :address        => 'smtp.gmail.com',
+    :address        => 'smtp.sendgrid.com',
     :port         => 587,
     :enable_starttls_auto => true,
-    :user_name          => 'twasden@gmail.com',
-    :password       => '0YlISEFTWTo2',
+    :user_name          => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :authentication     => :plain, # :plain, :login, :cram_md5, no auth by default
-    :domain         => 'arcane-citadel-8092.herokuapp.com'  
+    :domain         => 'herokuapp.com'  
 
   }
 )

2369a1dbbc632d5e9ea2a00a312be1fee9fd1173
twasden <wasdenonrails@gmail.com>
2014-11-09 09:37:25 -0600

    made title dynamic

--- app/controllers/abes_controller.rb
+++ app/controllers/abes_controller.rb
@@ -2,17 +2,26 @@ class AbesController < ApplicationController
 
   def home
     render layout: "home"
+    @title = "| home"
   end
 
   def who
+    @title = "| who we are"
   end
 
   def what
+    @title = "| what we do"
   end
 
   def power_point
+    @title = "| how we do it"
   end
 
   def current_status
+    @title = "| current status"
+  end
+
+  def news
+    @title = "| news"
   end
 end
--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,12 +1,14 @@
 class InquiriesController < ApplicationController
   def new
    @inquiry = Inquiry.new
+   @title = "| request info"
   end
   
   def create
     @inquiry = Inquiry.new(params[:inquiry])
     if @inquiry.deliver
       render :thank_you
+      @title = "| thank you"
     else
       render :new
     end
